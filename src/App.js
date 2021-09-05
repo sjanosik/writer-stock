1f73bb25821726f63b7a128a97ea74653291aa45
sjanosik <32128565+sjanosik@users.noreply.github.com>
2021-09-05 16:36:58 +0100

    Added React Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/stock">What's in Stock</Link>
+            </li>
+            <li>
+              <Link to="/saved">Saved Shuffles & Ideas</Link>
+            </li>
+            <li>
+              <Link to="/login">Sign up or Log in</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/stock">
+            <Stock />
+          </Route>
+          <Route path="/saved">
+            <Saved />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Stock() {
+  return <h2>What's in Stock</h2>
+}
+
+function Saved() {
+  return <h2>Saved Shuffles & Ideas</h2>
+}
+
+function Login() {
+  return <h2>Sign Up or Log In</h2>
 }
 
 export default App;
