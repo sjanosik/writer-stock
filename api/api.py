ceff8f889490acd18a190dfaf03b5d5f2ec80e47
sjanosik <32128565+sjanosik@users.noreply.github.com>
2021-09-05 21:39:36 +0100

    Add GetCategories Backend Function

--- api/api.py
+++ api/api.py
@@ -1,7 +1,27 @@
-from flask import Flask
+import os
+import sqlite3
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello world'
+
+@app.route('/pieces/getShuffleOptions')
+def get_shuffle_options():
+    params = request.args
+    include_base = params.get("includeBase", True)
+    include_user = params.get("includeUser", True)
+    user = params.get("userId")
+
+    db = sqlite3.connect(os.getenv('WRITERSTOCK_DATABASE_URI'))
+    rows = []
+    if include_base:
+        for row in db.execute('select name, description from category'):
+            rows.append({'name': row[0], 'description': row[1]})
+    if include_user and user:
+        for row in db.execute(f'select distinct category_id from piece where user_id = {user}'):
+            rows.append({'name': row[0], 'description': None})
+
+    print(f'Got categories: {rows}')
+
+    return {'categories': rows}
